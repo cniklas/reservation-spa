b64d633b03159f8c62b3c99b88002bddd524061e
Christian Niklas <christian.niklas@outletcity.com>
2024-02-14 15:03:35 +0100

    refactor dialog CSS

--- src/components/AppDialog.vue
+++ src/components/AppDialog.vue
@@ -10,7 +10,10 @@ defineExpose({ open })
 </script>
 
 <template>
-	<dialog ref="dialogEl">
+	<dialog
+		ref="dialogEl"
+		class="rounded-2 w-sm before:rounded-t-2 max-w-[calc(100vw-1.5rem)] border border-black p-3 pt-14 backdrop:bg-black/30 before:absolute before:left-0 before:top-0 before:h-12 before:w-full before:border-b before:border-b-black before:bg-gray-200 before:content-['']"
+	>
 		<button
 			type="button"
 			class="rounded-50% absolute right-0 top-0 inline-grid h-8 w-8 -translate-x-1/4 translate-y-1/4 place-content-center bg-slate-800 text-white focus-visible:outline-slate-800"
@@ -24,18 +27,3 @@ defineExpose({ open })
 		<div class="whitespace-pre-line first-line:font-semibold"><slot /></div>
 	</dialog>
 </template>
-
-<style lang="postcss">
-dialog {
-	@apply color-inherit rounded-2 w-sm fixed max-w-[calc(100vw-1.5rem)] border border-black p-3 pt-14;
-
-	&::before {
-		content: '';
-		@apply rounded-t-2 absolute left-0 top-0 h-12 w-full border-b border-b-black bg-gray-200;
-	}
-
-	&::backdrop {
-		@apply fixed inset-0 bg-black/30;
-	}
-}
-</style>
