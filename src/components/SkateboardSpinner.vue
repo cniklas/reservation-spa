ce9353f5ee9b0c1cb55dc9d317491044b188eb48
Christian Niklas <christian.niklas@outletcity.com>
2025-10-23 21:50:48 +0200

    use modern color syntax

--- src/components/SkateboardSpinner.vue
+++ src/components/SkateboardSpinner.vue
@@ -2,18 +2,18 @@
 	<svg viewBox="0 0 176 160" width="176" height="160" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
 		<defs>
 			<linearGradient id="pl-grad" x1="0" y1="0" x2="0" y2="1">
-				<stop offset="0%" stop-color="hsl(33,90%,55%)" />
-				<stop offset="30%" stop-color="hsl(33,90%,55%)" />
-				<stop offset="100%" stop-color="hsl(3,90%,55%)" />
+				<stop offset="0%" stop-color="hsl(33 90% 55%)" />
+				<stop offset="30%" stop-color="hsl(33 90% 55%)" />
+				<stop offset="100%" stop-color="hsl(3 90% 55%)" />
 			</linearGradient>
 		</defs>
 		<g fill="none" stroke-width="16" stroke-linecap="round">
-			<circle class="pl__ring" r="56" cx="88" cy="96" stroke="hsla(0,10%,10%,0.1)" />
+			<circle class="pl__ring" r="56" cx="88" cy="96" stroke="hsl(0 10% 10% / 10%)" />
 			<path class="pl__worm1" d="M144,96A56,56,0,0,1,32,96" stroke="url(#pl-grad)" stroke-dasharray="43.98 307.87" />
 			<path
 				class="pl__worm2"
 				d="M32,136V96s-.275-25.725,14-40"
-				stroke="hsl(33,90%,55%)"
+				stroke="hsl(33 90% 55%)"
 				stroke-dasharray="0 40 0 44"
 				stroke-dashoffset="0.001"
 				visibility="hidden"
@@ -21,7 +21,7 @@
 			<path
 				class="pl__worm3"
 				d="M144,136V96s.275-25.725-14-40"
-				stroke="hsl(33,90%,55%)"
+				stroke="hsl(33 90% 55%)"
 				stroke-dasharray="0 40 0 44"
 				stroke-dashoffset="0.001"
 				visibility="hidden"
@@ -45,7 +45,7 @@
 }
 
 .pl__ring {
-	stroke: hsla(var(--hue), 10%, 10%, 0.1);
+	stroke: hsl(var(--hue) 10% 10% / 10%);
 }
 
 .pl__worm1 {
